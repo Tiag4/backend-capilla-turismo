cb942469073424525d0555ac1d2e7c41bb550261
Tiag4 <longonjulian@gmail.com>
2026-09-12 06:37:30 -0300

    feat: configuracion de estructura inicial de paquetes y conexion a base de datos

---
+++ src/main/java/com/upc/demo/PortalTurismoApplication.java
@@ -0,0 +1,13 @@
+package com.upc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalTurismoApplication.class, args);
+	}
+
+}
